e8509d69b90ec3752ff57c92145d9b09eebe2152
ManmohanGupta <manmohan852@gmail.com>
2022-11-22 18:54:57 +0530

    solved rotten oranges

--- leetcode/src/RottenOranges.java
+++ leetcode/src/RottenOranges.java
@@ -1,9 +1,18 @@
 public class RottenOranges {
 
-
+//https://leetcode.com/problems/rotting-oranges/description/
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int [][] arr  = n ;
+        int [][] arr  = new int[3][3] ;
+        arr[0][0] =2;
+        arr[0][1] =1;
+        arr[0][2] =1;
+        arr[1][0] =1;
+        arr[1][1] =1;
+        arr[1][2] =0;
+        arr[2][0] =0;
+        arr[2][1] =1;
+        arr[2][2] = 1;
         solution.orangesRotting(arr);
     }
 
